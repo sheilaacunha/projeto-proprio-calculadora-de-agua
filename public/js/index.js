aeb5b25466f968490a61e069f638592904f1005c
Sheila Acunha <johndoe@example.comsheilaacunha1990@gmail.com>
2023-01-23 09:01:44 -0300

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,42 @@
+
+const pesoForm = document.getElementById('pesoForm');
+let inputPeso = document.getElementById('peso');
+let inputIdade = document.getElementById('idade');
+const divMsg = document.getElementById('msg');
+
+pesoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    calcular();
+    pesoForm.reset();
+
+});
+
+function calcular() {
+
+    if (inputIdade.value <= 0 || inputPeso.value <= 0) {
+        divMsg.innerHTML = `<span>Você deve colocar numeros acima de 0.</span>`
+
+    } else if (inputIdade.value <= 17) {
+        let qtdAgua = ((40 * inputPeso.value) / 1000).toFixed(3)
+        console.log(qtdAgua);
+
+        divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span> `
+    } else if (inputIdade.value >= 18 && inputIdade.value <= 55) {
+        let qtdAgua = ((35 * inputPeso.value) / 1000).toFixed(3)
+        console.log(qtdAgua);
+        divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span> `
+
+    } else if (inputIdade.value > 55 && inputIdade.value <= 65) {
+        let qtdAgua = ((30 * inputPeso.value) / 1000).toFixed(3)
+        console.log(qtdAgua);
+        divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span> `
+    } else if (inputIdade.value > 65) {
+        let qtdAgua = ((25 * inputPeso.value) / 1000).toFixed(3)
+        console.log(qtdAgua);
+        divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span>`
+
+    }
+
+
+
+}
