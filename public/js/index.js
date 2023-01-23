486775072f5da730be6065f5b5ec75db57e4c516
Sheila Acunha <johndoe@example.comsheilaacunha1990@gmail.com>
2023-01-23 10:03:20 -0300

    ajuste

--- public/js/index.js
+++ public/js/index.js
@@ -17,21 +17,21 @@ function calcular() {
         divMsg.innerHTML = `<span>Você deve colocar numeros acima de 0.</span>`
 
     } else if (inputIdade.value <= 17) {
-        let qtdAgua = ((40 * inputPeso.value) / 1000).toFixed(3)
+        let qtdAgua = ((40 * inputPeso.value) / 1000).toFixed(2)
         console.log(qtdAgua);
 
         divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span> `
     } else if (inputIdade.value >= 18 && inputIdade.value <= 55) {
-        let qtdAgua = ((35 * inputPeso.value) / 1000).toFixed(3)
+        let qtdAgua = ((35 * inputPeso.value) / 1000).toFixed(2)
         console.log(qtdAgua);
         divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span> `
 
     } else if (inputIdade.value > 55 && inputIdade.value <= 65) {
-        let qtdAgua = ((30 * inputPeso.value) / 1000).toFixed(3)
+        let qtdAgua = ((30 * inputPeso.value) / 1000).toFixed(2)
         console.log(qtdAgua);
         divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span> `
     } else if (inputIdade.value > 65) {
-        let qtdAgua = ((25 * inputPeso.value) / 1000).toFixed(3)
+        let qtdAgua = ((25 * inputPeso.value) / 1000).toFixed(2)
         console.log(qtdAgua);
         divMsg.innerHTML = `<span>Quantidade recomendada por kg é de ${qtdAgua} litros por dia !!!!</span>`
 
